bb5e5d1a8b937813d4068cf0c5ad964aea0c0749
Michael Arntzenius <daekharel@gmail.com>
2012-03-28 15:39:04 -0400

    rewrite to use vtables

--- slz.c
+++ slz.c
@@ -1,9 +1,15 @@
+/* feature test macro to get XSI-compliant strerror_r */
+#define _POSIX_C_SOURCE 200112L
+
 #include "slz.h"
 
 #include <errno.h>
 #include <math.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define ARRAY_LEN(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 /* The magic bytes (not including version identifier) that we expect at the
  * beginning of a slz value.
@@ -23,7 +29,7 @@ static const char version_string[] =
 
 static void slz_reraise(slz_ctx_t *ctx)
 {
-    assert (slz_error(ctx));
+    assert (!slz_ok(ctx));
     if (ctx->have_env) {
         ctx->have_env = false;
         longjmp(ctx->env, 1);
@@ -44,19 +50,28 @@ static void slz_raise(slz_ctx_t *ctx, slz_origin_t origin_type, void *origin)
     slz_reraise(ctx);
 }
 
-static void perrorish(const char *s, const char *errdesc)
-{
-    if (s) fprintf(stderr, "%s: ", s);
-    fprintf(stderr, "%s\n", errdesc);
-    fflush(stderr);
+static void *slz_malloc(slz_ctx_t *ctx, size_t sz) {
+    void *p = malloc(sz);
+    if (!p) {
+        ctx->state = SLZ_OOM;
+        slz_reraise(ctx);
+    }
+    assert (p);
+    return p;
 }
 
-/* Some forward declarations. */
-static bool try_expect_bytes(
-    slz_ctx_t *ctx, slz_src_t *src, size_t len, const char *data);
+static void perrorish(const char *s, const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
 
-static bool try_get_bytes(
-    slz_ctx_t *ctx, slz_src_t *src, size_t len, char *out);
+    if (s) fprintf(stderr, "%s: ", s);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    fflush(stderr);
+
+    va_end(ap);
+}
 
 
 /* Miscellany. */
@@ -68,13 +83,22 @@ slz_version_t slz_version(void)
                 });
 }
 
+bool slz_compatible_version(slz_version_t version)
+{
+    /* TODO: be more relaxed about this */
+    return (version.major == SLZ_VERSION_MAJOR &&
+            version.minor == SLZ_VERSION_MINOR &&
+            version.bugfix == SLZ_VERSION_BUGFIX);
+}
+
 
-/* Error handling. */
+/* slz_ctx_t management */
 void slz_init(slz_ctx_t *ctx,
               void (*handler)(slz_ctx_t*, void*),
               void *userdata)
 {
-    ctx->state = SLZ_NO_ERROR;
+    assert (handler);
+    ctx->state = SLZ_OK;
     ctx->have_env = false;
     ctx->toplevel_error_handler = handler;
     ctx->userdata = userdata;
@@ -89,16 +113,81 @@ void slz_init_with_perror(slz_ctx_t *ctx, const char *s) {
 }
 
 void slz_clear_error(slz_ctx_t *ctx) {
-    ctx->state = SLZ_NO_ERROR;
+    ctx->state = SLZ_OK;
+}
+
+
+/* Oh my god error message printing oh my god */
+#define INIT_PERROR_BUF_SIZE 100
+
+static char *xrealloc(const char *s, char *p, size_t len)
+{
+    char *pn = realloc(p, len);
+    if (!pn) {
+        perrorish(s,
+                  "libslz: error occurred, but could not allocate space "
+                  "for error message!");
+        free(p);
+    }
+    return pn;
+}
+
+static void do_perror(
+    const char *s,
+    size_t (*strerrorfunc)(void *obj, char *buf, size_t buflen), void *obj)
+{
+    static const size_t sbuflen = INIT_PERROR_BUF_SIZE;
+    char sbuf[sbuflen];
+    char *p = sbuf;
+    bool freeit = false;
+
+    size_t needed = strerrorfunc(obj, sbuf, sbuflen);
+    if (needed) {
+        if (needed < SIZE_MAX) {
+            if (!(p = xrealloc(s, NULL, needed)))
+                return;
+            freeit = true;
+            if (strerrorfunc(obj, p, needed)) {
+                free(p);
+                freeit = false;
+                p = "unknown error; underlying IO mechanism "
+                    "being inconsistent about error message length";
+            }
+        }
+        else {
+            p = NULL;
+            freeit = true;
+            size_t buflen = needed;
+            do {
+                if (buflen * 2 < buflen) {
+                    freeit = false;
+                    p = "unknown error; error message length overflows!";
+                    break;
+                }
+                if (!(p = xrealloc(s, p, buflen *= 2)))
+                    return;
+            } while (strerrorfunc(obj, p, buflen));
+        }
+    }
+
+    assert(p);
+    perrorish(s, "libslz: %s", p);
+    if (freeit) free(p);
 }
 
 void slz_perror(slz_ctx_t *ctx, const char *s)
 {
-    assert (slz_error(ctx));
-    switch ((slz_state_t) ctx->state) {
-      case SLZ_ERRNO:
-        errno = ctx->info.saved_errno;
-        perror(s);
+    assert (!slz_ok(ctx));
+    switch ((enum slz_state) ctx->state) {
+      case SLZ_IO_ERROR:
+        if (ctx->origin_type == SLZ_SRC)
+            do_perror(s, ctx->origin.src->funcs->strerror,
+                      ctx->origin.src->obj);
+        else {
+            assert(ctx->origin_type == SLZ_SINK);
+            do_perror(s, ctx->origin.sink->funcs->strerror,
+                      ctx->origin.sink->obj);
+        }
         break;
 
       case SLZ_BAD_HEADER:
@@ -106,109 +195,129 @@ void slz_perror(slz_ctx_t *ctx, const char *s)
         perrorish(s, "libslz: bad magic number or malformed header");
         break;
 
-      case SLZ_VERSION_MISMATCH:
-        assert (ctx->origin_type == SLZ_SRC);
-        perrorish(s, "libslz: version mismatch when deserializing");
-        break;
-
-      case SLZ_UNKNOWN_ERROR:
-        perrorish(s, "libslz: Unknown I/O error! :(");
-        break;
-
       case SLZ_UNFULFILLED_EXPECTATIONS:
         /* TODO: better error message */
         perrorish(s, "libslz: unexpected value");
         break;
 
-      case SLZ_NO_ERROR: IMPOSSIBLE;
+      case SLZ_OOM:
+        perrorish(s, "libslz: out of memory");
+        break;
+
+      case SLZ_OK: IMPOSSIBLE;
     }
 }
 
 void slz_end_catch(slz_ctx_t *ctx)
 {
-    assert (ctx->have_env && !slz_error(ctx));
+    assert (ctx->have_env && slz_ok(ctx));
     ctx->have_env = false;
 }
 
 
-/* Source & sink initialization. */
-static inline bool get_version_frag(
-    slz_ctx_t *ctx, slz_src_t *src, uint16_t *nump, char *cp)
+/* Initializing & destroying sources & sinks. */
+void slz_src_init(
+    slz_ctx_t *ctx, slz_src_t *src, slz_src_funcs_t *funcs, void *obj)
 {
-    assert (*nump == 0);
-    for (;;) {
-        if (!try_get_bytes(ctx, src, 1, cp))
-            return false;
-        if (*cp < '0' || '9' < *cp)
-            return true;
-        *nump = *nump * 10 + (*cp - '0');
-    }
+    assert (funcs);
+    src->error = false;
+    src->funcs = funcs;
+    src->obj = obj;
+    (void) ctx;                 /* unused */
 }
 
-static inline bool okay_version_number(
-     uint16_t major, uint16_t minor, uint16_t bugfix)
+void slz_sink_init(
+    slz_ctx_t *ctx, slz_sink_t *sink, slz_sink_funcs_t *funcs, void *obj)
 {
-    /* TODO: be more relaxed about this */
-    return (major == SLZ_VERSION_MAJOR &&
-            minor == SLZ_VERSION_MINOR &&
-            bugfix == SLZ_VERSION_BUGFIX);
+    assert (funcs);
+    sink->error = false;
+    sink->funcs = funcs;
+    sink->obj = obj;
+    (void) ctx;                 /* unused */
 }
 
-static void expect_header(slz_ctx_t *ctx, slz_src_t *src)
-{
-    char c;
-    uint16_t major = 0, minor = 0, bugfix = 0;
-    if (!(try_expect_bytes(ctx, src, strlen(magic), magic) &&
-          get_version_frag(ctx, src, &major, &c) && c == '.' &&
-          get_version_frag(ctx, src, &minor, &c) && c == '.' &&
-          get_version_frag(ctx, src, &bugfix, &c) && c == '\0')) {
-        ctx->state = SLZ_BAD_HEADER;
-        slz_raise(ctx, SLZ_SRC, src);
-    }
-
-    /* Check version number is the one we expect. */
-    if (!okay_version_number(major, minor, bugfix)) {
-        ctx->state = SLZ_VERSION_MISMATCH;
-        ctx->info.version = ((slz_version_t) {
-                .major = major, .minor = minor, .bugfix = bugfix });
-        slz_raise(ctx, SLZ_SRC, src);
-    }
+void slz_src_destroy(slz_ctx_t *ctx, slz_src_t *src) {
+    src->funcs->free(src->obj);
+    (void) ctx;
 }
 
+void slz_sink_destroy(slz_ctx_t *ctx, slz_sink_t *sink) {
+    sink->funcs->free(sink->obj);
+    (void) ctx;
+}
+
+
+/* File vtables and methods. */
+typedef struct { FILE *file; bool eof; int saved_errno; } file_t;
+
+static bool FILE_read(void *objp, char *buf, size_t buflen)
+{
+    file_t *obj = objp;
+    size_t read = fread(buf, 1, buflen, obj->file);
+    if (read != buflen) {
+        if (feof(obj->file)) obj->eof = true;
+        else obj->saved_errno = errno;
+        return false;
+    }
+    return true;
+}
+
+static bool FILE_write(void *objp, const char *buf, size_t buflen)
+{
+    file_t *obj = objp;
+    size_t written = fwrite(buf, 1, buflen, obj->file);
+    if (written != buflen) {
+        obj->saved_errno = errno;
+        return false;
+    }
+    return true;
+}
+
+static size_t FILE_strerror(void *objp, char *buf, size_t buflen)
+{
+    file_t *obj = objp;
+    if (obj->eof) {
+        static const char msg[] = "end-of-file reached";
+        if (buflen < ARRAY_LEN(msg))
+            return ARRAY_LEN(msg);
+        memcpy(buf, msg, ARRAY_LEN(msg));
+        return 0;
+    }
+
+    return strerror_r(obj->saved_errno, buf, buflen) ? SIZE_MAX : 0;
+}
+
+static slz_src_funcs_t FILE_src_funcs = {
+    .read = FILE_read,
+    .strerror = FILE_strerror,
+    .free = free
+};
+
+static slz_sink_funcs_t FILE_sink_funcs = {
+    .write = FILE_write,
+    .strerror = FILE_strerror,
+    .free = free
+};
+
+/* File initializers. */
 void slz_src_from_file(slz_ctx_t *ctx, slz_src_t *src, FILE *file)
 {
-    /* TODO: check that file is open for reading. */
+    file_t *f = slz_malloc(ctx, sizeof(file_t));
+    f->file = file;
+    f->eof = false;
+    slz_src_init(ctx, src, &FILE_src_funcs, (void*) f);
+    /* FIXME: check that file is open for reading. */
     /* FIXME: check file for error conditions. */
-    (void) ctx;
-    src->type = SLZ_SRC_FILE;
-    src->error = false;
-    src->src.file = file;
-    expect_header(ctx, src);
-}
-
-static inline size_t asciilen(uint16_t num)
-{
-    size_t r = 1;
-    while (num >= 10) num /= 10, ++r;
-    return r;
-}
-
-static void slz_put_header(slz_ctx_t *ctx, slz_sink_t *sink)
-{
-    /* Write magic number & version info. */
-    slz_put_bytes(ctx, sink, strlen(magic), magic);
-    /* sizeof rather than strlen to include the terminating null byte. */
-    slz_put_bytes(ctx, sink, sizeof version_string, version_string);
 }
 
 void slz_sink_from_file(slz_ctx_t *ctx, slz_sink_t *sink, FILE *file)
 {
-    /* TODO: check that file is open for writing */
+    file_t *f = slz_malloc(ctx, sizeof(file_t));
+    f->file = file;
+    f->eof = false;
+    /* FIXME: check that file is open for writing */
     /* FIXME: check file for error conditions */
-    sink->type = SLZ_SINK_FILE;
-    sink->error = false;
-    sink->sink.file = file;
-    slz_put_header(ctx, sink);
+    slz_sink_init(ctx, sink, &FILE_sink_funcs, (void*) f);
 }
 
 
@@ -216,23 +325,21 @@ void slz_sink_from_file(slz_ctx_t *ctx, slz_sink_t *sink, FILE *file)
 void slz_put_bytes(
     slz_ctx_t *ctx, slz_sink_t *sink, size_t len, const char *data)
 {
-    assert (!slz_error(ctx));
-    switch ((slz_sink_type_t) sink->type) {
-      case SLZ_SINK_FILE: (void) 0;
-        FILE *f = sink->sink.file;
-        size_t written = fwrite(data, 1, len, f);
-        if (written != len) {
-            /* Error! */
-            if (ferror(f)) {
-                ctx->state = SLZ_ERRNO;
-                ctx->info.saved_errno = errno;
-            }
-            else
-                ctx->state = SLZ_UNKNOWN_ERROR;
-            slz_raise(ctx, SLZ_SINK, sink);
-        }
-        break;
+    assert (slz_ok(ctx));
+    assert (!sink->error);
+
+    if (!sink->funcs->write(sink->obj, data, len)) {
+        ctx->state = SLZ_IO_ERROR;
+        slz_raise(ctx, SLZ_SINK, sink);
     }
+}
+
+void slz_put_magic(slz_ctx_t *ctx, slz_sink_t *sink)
+{
+    /* Write magic number & version info. */
+    slz_put_bytes(ctx, sink, strlen(magic), magic);
+    /* sizeof rather than strlen to include the terminating null byte. */
+    slz_put_bytes(ctx, sink, sizeof version_string, version_string);
 }
 
 void slz_put_bool(slz_ctx_t *ctx, slz_sink_t *sink, bool val)
@@ -298,26 +405,16 @@ void slz_put_int64(slz_ctx_t *ctx, slz_sink_t *sink, int64_t val)
 static bool try_get_bytes(
     slz_ctx_t *ctx, slz_src_t *src, size_t len, char *out)
 {
-    assert (!slz_error(ctx));
-    switch ((slz_src_type_t) src->type) {
-      case SLZ_SRC_FILE: (void) 0;
-        FILE *f = src->src.file;
-        size_t read = fread(out, 1, len, f);
-        if (read != len) {
-            /* Error! */
-            ctx->origin_type = SLZ_SRC;
-            ctx->origin.src = src;
-            if (feof(f) || ferror(f)) {
-                ctx->state = SLZ_ERRNO;
-                ctx->info.saved_errno = errno;
-            }
-            else
-                ctx->state = SLZ_UNKNOWN_ERROR;
-            return false;
-        }
-        break;
-    }
-    return true;
+    assert (slz_ok(ctx));
+    assert (!src->error);
+
+    if (src->funcs->read(src->obj, out, len))
+        return true;
+
+    ctx->origin_type = SLZ_SRC;
+    ctx->origin.src = src;
+    ctx->state = SLZ_IO_ERROR;
+    return false;
 }
 
 static bool try_expect_bytes(
@@ -343,14 +440,47 @@ void slz_get_bytes(slz_ctx_t *ctx, slz_src_t *src, size_t len, char *out)
 }
 
 void slz_expect_bytes(
-    slz_ctx_t *ctx, slz_src_t *src, size_t len, const char *data,
-    void *user_info)
+    slz_ctx_t *ctx, slz_src_t *src, size_t len, const char *data)
 {
     if (try_expect_bytes(ctx, src, len, data))
         return;
     ctx->state = SLZ_UNFULFILLED_EXPECTATIONS;
-    ctx->info.user_info = user_info;
     slz_reraise(ctx);
+}
+
+static inline bool get_version_frag(
+    slz_ctx_t *ctx, slz_src_t *src, uint16_t *nump, char *cp)
+{
+    assert (*nump == 0);
+    for (;;) {
+        if (!try_get_bytes(ctx, src, 1, cp))
+            return false;
+        if (*cp < '0' || '9' < *cp)
+            return true;
+        *nump = *nump * 10 + (*cp - '0');
+    }
+}
+
+slz_version_t slz_get_magic(slz_ctx_t *ctx, slz_src_t *src)
+{
+    char c;
+    slz_version_t v = {0, 0, 0};
+    if (!(try_expect_bytes(ctx, src, strlen(magic), magic) &&
+          get_version_frag(ctx, src, &v.major, &c) && c == '.' &&
+          get_version_frag(ctx, src, &v.minor, &c) && c == '.' &&
+          get_version_frag(ctx, src, &v.bugfix, &c) && c == '\0')) {
+        ctx->state = SLZ_BAD_HEADER;
+        slz_raise(ctx, SLZ_SRC, src);
+    }
+    return v;
+}
+
+void slz_expect_magic(slz_ctx_t *ctx, slz_src_t *src)
+{
+    if (!slz_compatible_version(slz_get_magic(ctx, src))) {
+        ctx->state = SLZ_UNFULFILLED_EXPECTATIONS;
+        slz_raise(ctx, SLZ_SRC, src);
+    }
 }
 
 /* TODO: many of these functions are dubiously portable. */
